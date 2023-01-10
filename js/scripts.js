d015092d0f389a331392c47746cdc6541dc900e4
leandro romero <leandro.romero06@outlook.es>
2023-01-09 17:00:27 -1200

    PreEntrega2-Romero

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// const botonCheckboxElement = document.getElementById('boton-checkbox');
+// const mainMenuElement = document.getElementById('main-menu')
+
+// botonCheckboxElement.addEventListener('click', ()=> {
+//     mainMenuElement.classList.boton('main-menu--show')
+// })
